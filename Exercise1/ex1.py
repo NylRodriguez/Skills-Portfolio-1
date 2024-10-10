406710f0a9977486ded58d5946ac6b078a695859
NylRodriguez <nylcyrus.rodriguez@gmail.com>
2024-10-10 19:28:30 +0400

    ex1 completed

---
+++ Exercise1/ex1.py
@@ -0,0 +1,7 @@
+#Assigning value to variable and make the statement "Coding is Cool"
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+
+#To get the reserves on the console.
+print(word1 + " " + word2 + " " + word3)
